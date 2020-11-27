321950f4e252ee470524ea50d51a4b9001055b3a
RapDoodle <bowenbhwu@gmail.com>
2020-11-28 03:03:40 +0800

    Preliminary support for group chat (1 group only)

--- prototype/src/server/server/server.cpp
+++ prototype/src/server/server/server.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <winsock2.h>
+#include <windows.h>
 #include <iostream>
 #include <iomanip>
 #include <chrono>
@@ -9,16 +10,21 @@
 
 #define VERSION "0.1 beta";
 #define DEFAULT_PORT 19135
-#define QUEUE_SIZE 32
+#define QUEUE_SIZE 16
+#define CON_CLIENTS 128
+#define MSG_LEN 140
 
 using namespace std;
 
-int app();
+void app();
 void consoleLog(string str);
 string getCurrentTimeString();
+BOOL WINAPI intrHandler(DWORD signal);
 
 struct sockaddr_in srv;
-fd_set fr, fw, fe;
+
+int nMaxFd = 0;
+int clients[CON_CLIENTS];
 
 int mainSock;
 
@@ -40,8 +46,12 @@ int main()
     app();
 }
 
-int app()
+void app()
 {
+    /* Initialize interrupt handler */
+    if (!SetConsoleCtrlHandler(intrHandler, TRUE))
+        consoleLog("WARNING: Unable to setup interrupt handler");
+
     /* Initialize the WSA variables for Windows */
     WSADATA ws;
     if (WSAStartup(MAKEWORD(2, 2), &ws) < 0) {
@@ -68,6 +78,15 @@ int app()
     srv.sin_addr.s_addr = INADDR_ANY;
     memset(&srv.sin_zero, 0, 8);
 
+    // setsockopt
+    int nOptVal = 0;
+    int nOptLen = sizeof(nOptVal);
+    if (setsockopt(mainSock, SOL_SOCKET, SO_REUSEADDR, (const char*)&nOptVal, nOptLen)) {
+        cout << "Fail to call setsockopt." << endl;
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+
     /* Set the socket as a non-blocking TCP socket */
     /* 0 for blocking and 1 for non-blocking */
     u_long opt = 1;
@@ -93,9 +112,113 @@ int app()
         consoleLog("Currently listening on port 19135");
     }
 
-    consoleLog("Server shutting down...");
-    WSACleanup();
-    return 0;
+    nMaxFd = mainSock;
+    struct timeval tv;
+    tv.tv_usec = 1;
+    tv.tv_usec = 0;
+
+    fd_set fr, fw, fe;
+
+    /* Spin up the server */
+    while (true) {
+
+        /* File descriptors */
+        FD_ZERO(&fr);   /* Read */
+        FD_ZERO(&fw);   /* Write */
+        FD_ZERO(&fe);   /* Exceptions */
+
+        /* Read from the read and exception descriptor */
+        FD_SET(mainSock, &fr);
+        FD_SET(mainSock, &fe);
+
+        for (int i = 0; i < CON_CLIENTS; i++) {
+            if (clients[i] != 0) {
+                FD_SET(clients[i], &fr);
+                FD_SET(clients[i], &fe);
+            }
+        }
+
+        int rtOpt = select(nMaxFd + 1, &fr, &fw, &fe, &tv);
+
+        if (rtOpt > 0) {
+            
+            /* When data arrived at the port */
+            if (FD_ISSET(mainSock, &fe)) {
+                /* Check for exceptions */
+                consoleLog("WARNING: Exception occurred");
+            } else if (FD_ISSET(mainSock, &fr)) {
+                /* Valid incoming request */
+                int nLen = sizeof(struct sockaddr);
+                int currSock = accept(mainSock, NULL, &nLen);
+
+                if (currSock > 0) {
+                    /* New socket connection */
+                    int i;
+                    for (i = 0; i < CON_CLIENTS; i++) {
+                        if (clients[i] == 0) {
+                            clients[i] = currSock;
+                            send(currSock, "NACK", 5, 0);
+                            break;
+                        }
+                    }
+
+                    if (i == CON_CLIENTS)
+                        consoleLog("WARNING: Maximum concurrent connection reached");
+                }
+            } else {
+                for (int i = 0; i < CON_CLIENTS; i++) {
+                    char recvBuff[MSG_LEN + 1];
+
+                    if (FD_ISSET(clients[i], &fr)) {
+                        if (recv(clients[i], recvBuff, MSG_LEN + 1, 0) < 0) {
+                            closesocket(clients[i]);
+                            clients[i] = 0;
+                        } else {
+                            /* handle the new message from the client */
+                            if (strlen(recvBuff) > 0) {
+                                cout << "[" << clients[i] << "]: " << recvBuff << endl;
+
+                                /* Broadcast the message */
+                                for (int j = 0; j < CON_CLIENTS; j++) {
+                                    if (clients[j] != 0 && i != j) {
+                                        try {
+                                            send(clients[j], recvBuff, 140, 0);
+                                        } catch (int e) {
+                                            consoleLog(to_string(e));
+                                        }
+                                    }
+                                }
+                            }
+                            send(clients[i], "ACK", 4, 0);
+                        }
+                    } 
+                }
+            }
+
+        } else if (rtOpt == 0) {
+            /* No connection */
+        } else {
+            /* Exception on the port */
+            consoleLog("Error on the port");
+            consoleLog(to_string(GetLastError()));
+            consoleLog("Shutting down the server...");
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        /* Avoid busy waiting */
+        Sleep(1);
+    }
+
+    /* Any code below should never be executed */
+}
+
+BOOL WINAPI intrHandler(DWORD signal) {
+    if (signal == CTRL_C_EVENT) {
+        consoleLog("Server shutting down...");
+        WSACleanup();
+    }
+    exit(0);
 }
 
 void consoleLog(string str)
